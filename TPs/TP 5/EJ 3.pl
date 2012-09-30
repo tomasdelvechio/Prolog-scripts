c49ccd46e0a6fa147366d178f495f2219404c8b2
Tomas <tomas@nesys.com.ar>
2012-09-30 18:20:03 -0300

    Terminado Ej 3

--- TPs/TP 5/EJ 3.pl
+++ TPs/TP 5/EJ 3.pl
@@ -10,6 +10,8 @@
 
 %    3.* Usando la base de datos generada en el problema de la familia de Luis, y con lo que se conoce hasta ahora, escribir consultas para obtener la siguiente información:
 
+consult('EJ 2').
+
 %    Preguntas simples:
 
 %    a) ¿Es Carlos progenitor de Guillermo?
@@ -38,14 +40,47 @@ es_progenitor_de(carlos,amalia).
 
 %    d) Usando el predicado "padre" preguntar: ¿Es Carlos padre de Roberto?
 
-padre(X,Y) :-
-    es_progenitor_de(X,Y).
+padre(X,Y) :- es_progenitor_de(X,Y).
+
+%Para cargar la regla en Prolog:
+%?- [user].
+%padre(X,Y) :- es_progenitor_de(X,Y).
+% APRETAR Ctrl+D para insertar un EOF.
+%|: % user://1 compiled 0.01 sec, 412 bytes
+%true.
+
+padre(carlos,roberto).
+%% EJECUCION
+%?- padre(carlos,roberto).
+%true.
 
 %    e) ¿Gonzalo es mujer?
+
+es_mujer(gonzalo).
+
+%?- es_mujer(gonzalo).
+%false.
+
 %    f) ¿Alberto es varón?
+
+es_varon(alberto).
+
+%?- es_varon(alberto).
+%false.
 
 %    Preguntas compuestas (conjunciones):
 
 %    g) ¿Es cierto que Luis es progenitor de Carla, y que Carla es mujer?.
+
+es_progenitor_de(luis,carla),es_mujer(carla).
+
+%?- es_progenitor_de(luis,carla),es_mujer(carla).
+%true.
+
 %    h) ¿Es cierto que Ernesto es progenitor de Carlos, y que éste es progenitor de Luis, y a su vez Luis es progenitor de Carla?
+
+es_progenitor_de(ernesto,carlos), es_progenitor_de(carlos,luis), es_progenitor_de(luis,carla).
+
+%?- es_progenitor_de(ernesto,carlos),es_progenitor_de(carlos,luis),es_progenitor_de(luis,carla).
+%false.
 
